e2a3675e22849dd453d81e4a6cde8725eb9881d8
Stepan Zabelin <stepanzabelin@gmail.com>
2023-07-29 13:36:26 +0200

    refactored validation and transformation

--- src/test-cli-app/modules/migration/migration-from/migration-from-options.dto.ts
+++ src/test-cli-app/modules/migration/migration-from/migration-from-options.dto.ts
@@ -1,28 +1,25 @@
 import { Validate, Clirio, Option } from '@clirio';
+const { OPTIONAL, NULLABLE, STRING, NUMBER, KEY_VALUE, FLAG } = Clirio.valid;
 
 export class MigrationFromOptionsDto {
   @Option('--env, -e')
-  @Validate(Clirio.valid.KEY_VALUE)
+  @Validate(KEY_VALUE)
   readonly envs?: Record<string, string>;
 
   @Option('--silent, -s')
-  @Validate(Clirio.valid.FLAG)
+  @Validate(FLAG)
   readonly silent?: boolean;
 
   @Option('--id, -i')
-  @Validate(Clirio.valid.NUMBER)
+  @Validate(NUMBER)
   readonly id!: number;
 
   @Option('--start-date, -b')
-  @Validate([Clirio.valid.NULLABLE, Clirio.valid.STRING])
+  @Validate([NULLABLE, STRING])
   readonly startDate!: string;
 
-  @Option('--start-date, -b')
-  @Validate([Clirio.valid.NULLABLE, Clirio.valid.STRING])
-  readonly startDate2!: string;
-
   @Option('--end-date, -e')
-  @Validate([Clirio.valid.OPTIONAL, Clirio.valid.STRING])
+  @Validate([OPTIONAL, STRING])
   readonly endDate?: string;
 
   @Option('--algorithm, -a')
