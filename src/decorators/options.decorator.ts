5ed383920635cfc3e2eb380aa2552a308a2e18c1
Stepan Zabelin <stepanzabelin@gmail.com>
2023-08-22 00:52:53 +0200

    fixed types for parameter decorators

--- src/decorators/options.decorator.ts
+++ src/decorators/options.decorator.ts
@@ -1,13 +1,9 @@
 import { ArgMetadata } from '../classes';
 import { optionsArgMetadata } from '../metadata';
-import { Constructor, InputTypeEnum } from '../types';
+import { InputTypeEnum } from '../types';
 
 export const Options = function () {
-  return function (
-    target: Constructor<any>,
-    propertyName: string,
-    argIndex: number,
-  ) {
+  return function (target: object, propertyName: string, argIndex: number) {
     const entity = ArgMetadata.extractEntity(target, propertyName, argIndex);
 
     optionsArgMetadata.setArgData(target, propertyName, argIndex, {
--- src/decorators/params.decorator.ts
+++ src/decorators/params.decorator.ts
@@ -1,13 +1,9 @@
 import { ArgMetadata } from '../classes';
 import { paramsArgMetadata } from '../metadata';
-import { Constructor, InputTypeEnum } from '../types';
+import { InputTypeEnum } from '../types';
 
 export const Params = function () {
-  return function (
-    target: Constructor<any>,
-    propertyName: string,
-    argIndex: number,
-  ) {
+  return function (target: object, propertyName: string, argIndex: number) {
     const entity = ArgMetadata.extractEntity(target, propertyName, argIndex);
 
     paramsArgMetadata.setArgData(target, propertyName, argIndex, {
